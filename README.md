7774efd17b5fd640f682ef1d64a5e19bc826c15c
John He <johnhsq@126.com>
2024-01-22 15:18:02 +0800

    Create repo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PalWorld-SaveBackuper
+这个项目用于自动备份PalWorld的存档,每15分钟备份一次
+## 使用方法
+###  方法一:使用exe文件
+直接下载并使用该文件,这个程序会被WindowsDefender判定为病毒,如果不放心请直接使用源代码:
+
+https://github.com/Decikingship/PalWorld-SaveBackuper/releases/tag/Windows
+###  方法二:使用源代码
+1. 安装Python3.x(如已安装请跳过): https://www.python.org/downloads/
+2. 安装git(如已安装请跳过): https://git-scm.com/downloads
+3. 克隆本项目: `git clone https://github.com/Decikingship/PalWorld-SaveBackuper.git`
+4. 使用cmd进入项目目录。
+5. cmd执行安装依赖: `pip install -r requirements.txt`
+6. 运行本项目: `python backuper.py`
+7. 输入你的存档路径(比如`C:\Users\Administrator\Desktop\steamcmd\steamapps\common\PalServer\Pal\Saved\SaveGames\0`)并回车,这也是默认的存档路径。
+
+## License
+本项目基于Apache-2.0协议开源,可以随意修改。
---
+++ backuper.py
@@ -0,0 +1,37 @@
+import shutil
+import time
+import os
+from datetime import datetime
+
+# 源文件夹路径
+source_folder = ''
+while source_folder == '':
+    source_folder = input(r'输入存档所在文件夹路径,比如C:\Users\Administrator\Desktop\steamcmd\steamapps\common\PalServer\Pal\Saved\SaveGames\0: ').replace('"', '').strip()
+    if source_folder == '':
+        source_folder = r'C:\Users\Administrator\Desktop\steamcmd\steamapps\common\PalServer\Pal\Saved\SaveGames\0'
+
+# 备份文件夹的基础路径
+backup_base_folder = r'C:\Backups'
+
+# 无限循环，程序将持续运行
+while True:
+    # 检查备份基础文件夹是否存在，如果不存在，则创建它
+    if not os.path.exists(backup_base_folder):
+        os.makedirs(backup_base_folder)
+        print(f'备份文件夹{backup_base_folder}已创建.')
+
+    # 获取当前时间，并格式化为年月日时分秒
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    # 创建备份文件夹的路径
+    backup_folder = os.path.join(backup_base_folder, f'Backup_{timestamp}')
+
+    try:
+        # 使用shutil.copytree来复制整个文件夹
+        shutil.copytree(source_folder, backup_folder)
+        print(f'成功于{backup_folder}完成备份')
+    except Exception as e:
+        # 如果发生任何错误，打印错误信息
+        print(f'备份错误: {e}')
+
+    # 等待30分钟，1800秒
+    time.sleep(1800)
